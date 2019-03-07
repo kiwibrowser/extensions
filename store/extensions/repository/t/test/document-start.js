3c38016980f99f014967061d8ed48fdc99bb8d87
Kiwi Browser <40272275+kiwibrowser@users.noreply.github.com>
2019-03-07 13:57:12 +0200

    Update document-start.js

--- store/extensions/repository/t/test/document-start.js
+++ store/extensions/repository/t/test/document-start.js
@@ -1,5 +1,2 @@
-(() => {
-  'use strict';
-  
-  console.log('Running at document start');
-})();
+console.log('Running - Dumping current page status' + document.readyState);
+console.log('Running at document start on ' + document.location);
