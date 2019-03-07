3633b87e0f0fb5f243364abbd7021618f4b9f732
Kiwi Browser <40272275+kiwibrowser@users.noreply.github.com>
2019-03-07 12:41:06 +0200

    Update document-end.js

--- store/extensions/repository/t/test/document-end.js
+++ store/extensions/repository/t/test/document-end.js
@@ -1,5 +1,6 @@
 (() => {
   'use strict';
+  console.log('Dumping current page status' + document.readyState);
   alert('Testing context ' + document.location);
   console.log('Running at document end');
 })();
